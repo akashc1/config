52ea0c9c30d2d3cd5c3dad2a7ffae641183742b1
Akash Chaurasia <akashchaurasia17@yahoo.com>
2023-04-05 04:34:11 +0000

    Better shell logic in setup script

--- setup.sh
+++ setup.sh
@@ -5,15 +5,16 @@ echo "Copying all config files to $HOME"
 # Copy shell-specific config files
 if [[ $SHELL == *bash* ]]; then
     cp .bashrc .bash_aliases .bash_profile $HOME
-else
+elif [[ $SHELL == *zsh* ]]; then
     cp .zshrc .zsh_aliases .zsh_profile $HOME
+else
+    echo "Unable to identify shell! Exiting..."
+    kill -INT $$
 fi
 
+# Vim/tmux config files
 cp .tmux.conf .tmux.conf.local .vimrc $HOME
 cp -r .vim $HOME
-
-
-cd $HOME
 
 # install vim plugins
 vim +PluginInstall +qall
